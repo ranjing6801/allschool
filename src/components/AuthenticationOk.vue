f7661a7f1e99bdf3cf2725c9f0a3a7e555d46a84
System Administrator <root@ranjingdeMacBook-Air.local>
2018-04-03 15:03:49 +0800

    fix

--- src/components/AuthenticationOk.vue
+++ src/components/AuthenticationOk.vue
@@ -1,20 +1,20 @@
 <template>
-    <!-- 老用户输入姓名认证成功   -->
     <div id="authentication">  
       <div v-if="isIOS" class="ios">
         <div class="content" slot="header">
+            <p class="p-img"><img src="/static/images/smile.png" alt=""></p>
             <p class="title">反馈成功</p>
-            <p>我们将尽快处理,请耐心等候</p>
+            <p class="title1">我们将尽快处理,请耐心等候</p>
         </div>
         <div class="footer">
-            <p class="footerTip">您可以使用「小黑板」,体验高效的家校管理</p>
+            <p class="footerTip">您可以先使用【小黑板】，体验高效的家校管理</p>
             <div class="btn" @click="openSmallDesk">打开小黑板</div>
         </div>
       </div>
       <div v-if="isAND" class="android">
         <div class="content2">
             <p class="title">反馈成功</p>
-            <p>我们将尽快处理,请耐心等候</p>
+            <p class="title1">我们将尽快处理,请耐心等候</p>
         </div>
         <div class="footer2">
             <p class="footerTip">您可以使用「小黑板」,体验高效的家校管理</p>
@@ -65,74 +65,79 @@ export default {
 </script>
 
 <style scoped>
-/*// @import  '../assets/css/variables.scss';      
-// @import  '../assets/css/authentication.css';*/
-
 
 #authentication {
   width: 100%;
   height: 100%;
-  line-height: 100%;
   box-sizing: border-box;
 }
 
-#authentication .content {
+.content {
   width: 100%;
   height: 100%;
-  padding-top: 50%;
-  font-size: 20px;
-  text-align: center;
-  letter-spacing: 2px;
 }
 .content2{
   width: 100%;
   height: 100%;
-  padding-top: 10%;
-  font-size: 20px;
-  text-align: center;
-  letter-spacing: 2px;
 }
 
-#authentication .footer {
-  margin-top: 50%;
+.footer {
+
+}
+.footer .footerTip {
+  font-family: PingFangSC-Light;
+  font-size: 0.3733rem;
+  color: #AAAAAA;
+  line-height: 0.3733rem;
+  text-align: center;
 }
 .footer2{
-  margin-top: 2%;
-  margin-bottom: 4%;
+  
 }
 .footer2 .footerTip{
-  font-size: 14px;
-  letter-spacing: 1.5px;
+  color: #AAAAAA;
+  font-size: 0.1867rem;
   text-align: center;
-  color: #666;
+  font-family: PingFangSC-Light;
 }
 
-#authentication .footer .footerTip {
-  font-size: 14px;
-  letter-spacing: 1.5px;
-  margin-bottom: -5px;
-  text-align: center;
-  color: #666;
-}
+
 
 .btn {
-  font-size: 20px;
-  line-height: 40px;
+  width: 9.2rem;
+  height: 1.28rem;
+  line-height: 1.28rem;
+  color: #000000;
+  background: #F8E71C;
+  font-size: 0.4533rem;
+  border-radius: 0.0533rem;
   text-align: center;
-  color: #fff;
-  width: 80%;
-  height: 40px;
-  margin-left: 10%;
-  text-align: center;
-  background: rgba(0, 0, 0, 0.5);
-  border-radius: 20px;
-  margin-top: 0px;
+  font-family: PingFangSC-Regular;
 }
 .img{
   text-align: center;
 }
-
-
+.p-img{
+  text-align: center;
+}
+.p-img img{
+  width: 1.6rem;
+  height: 1.6rem;
+}
+.title{
+  color: #FFFFFF;
+  font-size: 0.5333rem;
+  line-height: 0.4533rem;
+  text-align: center;
+  font-family: PingFangSC-Light;
+}
+.title1{
+  color: #FFFFFF;
+  font-size: 0.5333rem;
+  line-height: 0.4533rem;
+  text-align: center;
+  font-family: PingFangSC-Light;
+}
 
 
 </style>
