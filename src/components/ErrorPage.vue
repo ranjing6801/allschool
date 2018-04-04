805f9e886d0c64a04669147e03838788295c5a85
ranjing6801 <ranjing6801@163.com>
2018-04-04 10:06:46 +0800

    fix

--- src/components/ErrorPage.vue
+++ src/components/ErrorPage.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="error">
         {{err}}
+        <p>{{msg}}</p>
     </div>
 </template>
 <script>
@@ -8,22 +9,24 @@ export default {
     name:'error',
     data(){
         return {
-          err:'{"error_response":{"code":201,"msg":"请重新扫描二维码"}}'
+          err:'{"error_response":{"code":201,"msg":"请重新扫描二维码"}}',
+          msg:''
         }
     },
     methods:{
         
     },
     mounted(){
-        document.title = '错误1'
+        document.title = '错误1';
+        this.msg = this.$route.query.msg;
     }
 }
 </script>
 
 <style scoped>
 #error{
-  font-size: 0.2133rem;
-  color: #333;
+  font-size: 0.4533rem;
+  color: #fff;
   padding: 0.1667rem 0 0 0.1667rem;
 }
 
