a114b49aefa54a99eca68ecb22d182a53f984112
CarolineJason <huanglin_jason@163.com>
2018-04-25 17:11:44 +0800

    'fix'

--- src/store/store.js
+++ src/store/store.js
@@ -5,10 +5,6 @@ Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   state:{
-      saveId:[] ,       // 保存整校班级id
-      selectData:[],    // 初始值和options一样 当已经有可以取消的对应班级的时候,保存剩下的还没有认证的班级
-      items:[],         // 可以取消认证的班级
-      havenClass:[],    // 已经认证过的班级
       res1:[],
       res2:[],      
       zxClass:[            // 整校班级
@@ -20,7 +16,7 @@ export const store = new Vuex.Store({
             {
               teamName:'阳光中队',  // 班级名称
               banjiNumber:'123654',   // 班级号
-              banjichuangjianzhe:'王姐姐',   //  班级创建者 如果不知道真实姓名下展示小黑板账号
+              banjichuangjianzhe:'王姐姐',  //  班级创建者 如果不知道真实姓名下展示小黑板账号
               Members:32,  // 班级中的人数
               pid:0 ,
               isbgShow:'',
