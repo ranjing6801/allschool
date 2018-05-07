f40bd21822df2f8d5ac4552de76235614e12dbc3
CarolineJason <huanglin_jason@163.com>
2018-05-07 16:45:07 +0800

    'fix'

--- src/components/CreateClass.vue
+++ src/components/CreateClass.vue
@@ -295,8 +295,11 @@ export default {
   margin-left: 0.5333rem;
   margin-right: 0.5333rem;
   position: absolute;
-  top: 4.8267rem;
+  top: 0;
   left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
 }
 
 /*  保存二维码弹窗 */
